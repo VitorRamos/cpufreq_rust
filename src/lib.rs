eab0ee23e8e55c329f9a89f8f7d90137502757c8
vitorramos <ramos.vitor89@gmail.com>
2023-08-24 21:40:06 +0200

    Add initial documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,35 @@
+//! Wrapper arround the cpufreq fs
 #![feature(test)]
 use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::Path;
 
+/// Cpufreq error type
 type CpuFreqError = Box<dyn std::error::Error>;
+
+/// Base cpufreq functionality for reading and writing on cpu variables
 pub trait CpuFreq {
+    // Base path to be defined
     const CPUFREQ_PATH: &'static str;
+    /// Read file on CPUFREQ_PATH
+    ///
+    /// # Panics
+    ///
+    /// Panics if the content of the file is not utf8 encoded
     fn read_file(fname: &str) -> Result<String, std::io::Error> {
         let path = Path::new(Self::CPUFREQ_PATH).join(fname);
         Ok(String::from_utf8(fs::read(path)?).unwrap())
     }
+    /// Write to file on CPUFREQ_PATH
     fn write_file(fname: &str, data: &str) -> Result<(), std::io::Error> {
         let path = Path::new(Self::CPUFREQ_PATH).join(fname);
         fs::write(path, data)
     }
+    /// Read and parse cpufreq ranges, example:
+    /// 0,4,6-12,18 -> \[0,4,6,7,8,9,10,12,18\]
+    ///
+    /// # Panics
+    /// Panics if values cannot be parsed to usize
     fn get_ranges(fname: &str) -> Result<Vec<usize>, std::io::Error> {
         let range = Self::read_file(fname)?;
         let mut l: Vec<usize> = Vec::new();
@@ -27,6 +43,7 @@ pub trait CpuFreq {
         }
         Ok(l)
     }
+    /// Read a specific variable and parse to T
     fn get_variable<T>(id: usize, var: &str) -> Result<T, CpuFreqError>
     where
         T: std::str::FromStr,
@@ -36,11 +53,12 @@ pub trait CpuFreq {
         let cpu_data = Self::read_file(&path)?;
         Ok(cpu_data.trim().parse()?)
     }
+    ///  Sets a specific variable
     fn set_variable(id: usize, var: &str, data: &str) -> Result<(), std::io::Error> {
         let path = format!("cpu{id}/cpufreq/{var}");
         Self::write_file(&path, data)
     }
-
+    /// Get variables for all online cpus
     fn get_variable_all<T>(var: &str) -> Result<HashMap<usize, T>, CpuFreqError>
     where
         T: std::str::FromStr,
@@ -52,6 +70,7 @@ pub trait CpuFreq {
         }
         Ok(data)
     }
+    /// Set variables for all online cpus
     fn set_variable_all(var: &str, data: &str) -> Result<(), std::io::Error> {
         for cpu in Self::get_ranges("online")? {
             let path = format!("cpu{cpu}/cpufreq/{var}");
@@ -61,14 +80,25 @@ pub trait CpuFreq {
     }
 }
 
-struct CPU {}
+/// CPU object
+pub struct CPU {}
 
 impl CpuFreq for CPU {
+    /// Base path for cpufreq
     const CPUFREQ_PATH: &'static str = "/sys/devices/system/cpu/";
 }
 
 impl CPU {
-    fn new() -> Result<Self, CpuFreqError> {
+    /// Creates a new CPU
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.frequencies().expect("Unable to read frequencies");
+    /// ```
+    pub fn new() -> Result<Self, CpuFreqError> {
         if std::env::consts::OS != "linux" {
             let err =
                 std::io::Error::new(std::io::ErrorKind::Unsupported, "Only supported on Linux");
@@ -86,52 +116,170 @@ impl CPU {
         };
         Ok(CPU {})
     }
-    fn online(&self) -> Result<Vec<usize>, CpuFreqError> {
+    /// Get online cpus
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.online().expect("Unable to read online cpus");
+    /// ```
+    pub fn online(&self) -> Result<Vec<usize>, CpuFreqError> {
         Ok(CPU::get_ranges("online")?)
     }
-    fn governors(&self) -> Result<HashMap<usize, String>, CpuFreqError> {
+    /// Get online governors
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.governors().expect("Unable to read online governors");
+    /// ```
+    pub fn governors(&self) -> Result<HashMap<usize, String>, CpuFreqError> {
         Ok(CPU::get_variable_all("scaling_governor")?)
     }
-    fn frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
+    /// Get online frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.frequencies().expect("Unable to read online frequencies");
+    /// ```
+    pub fn frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
         Ok(CPU::get_variable_all("scaling_cur_freq")?)
     }
-    fn max_frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
+    /// Get online max_frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.max_frequencies().expect("Unable to read online max_frequencies");
+    /// ```
+    pub fn max_frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
         Ok(CPU::get_variable_all("scaling_cur_freq")?)
     }
-    fn min_frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
+    /// Get online min_frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.min_frequencies().expect("Unable to read online min_frequencies");
+    /// ```
+    pub fn min_frequencies(&self) -> Result<HashMap<usize, u64>, CpuFreqError> {
         Ok(CPU::get_variable_all("scaling_cur_freq")?)
     }
-    fn available_frequencies(&self) -> Result<HashMap<usize, Vec<u64>>, CpuFreqError> {
+    /// Get online min_frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.min_frequencies().expect("Unable to read online min_frequencies");
+    /// ```
+    pub fn available_frequencies(&self) -> Result<HashMap<usize, Vec<u64>>, CpuFreqError> {
         let mut res = HashMap::new();
         for (cpu, freq) in CPU::get_variable_all::<String>("scaling_available_frequencies")? {
             res.insert(cpu, freq.split(" ").map(|x| x.parse().unwrap()).collect());
         }
         Ok(res)
     }
-    fn set_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
+    /// Set online cpu frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// cpu.set_governors("userspace");
+    /// let freqs = cpu.set_frequencies("2300000").expect("Unable to set frequencies");
+    /// ```
+    pub fn set_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
         CPU::set_variable_all("scaling_setspeed", &freq.to_string())?;
         CPU::set_variable_all("scaling_max_freq", &freq.to_string())?;
         CPU::set_variable_all("scaling_min_freq", &freq.to_string())?;
         Ok(())
     }
-    fn set_max_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
+    /// Set online cpu max possible frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.set_max_frequencies(2301000).expect("Unable to set max frequencies");
+    /// ```
+    pub fn set_max_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
         CPU::set_variable_all("scaling_max_freq", &freq.to_string())?;
         Ok(())
     }
-    fn set_min_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
+    /// Set online cpu min possible frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.set_min_frequencies(2301000).expect("Unable to set min frequencies");
+    /// ```
+    pub fn set_min_frequencies<T: ToString>(&self, freq: T) -> Result<(), CpuFreqError> {
         CPU::set_variable_all("scaling_min_freq", &freq.to_string())?;
         Ok(())
     }
-    fn set_governors(&self, gov: &str) -> Result<(), CpuFreqError> {
+    /// Set online cpu governors
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.set_governors("ondemand").expect("Unable to set governors");
+    /// ```
+    pub fn set_governors(&self, gov: &str) -> Result<(), CpuFreqError> {
         Ok(CPU::set_variable_all("scaling_governor", &gov)?)
     }
-    fn enable(&self, id: usize) -> Result<(), CpuFreqError> {
+    /// Enable one cpu
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.enable(5).expect("Unable enable cpu");
+    /// ```
+    pub fn enable(&self, id: usize) -> Result<(), CpuFreqError> {
         Ok(CPU::write_file(&format!("cpu{id}/online"), "1")?)
     }
-    fn disable(&self, id: usize) -> Result<(), CpuFreqError> {
+    /// Disable one cpu
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.disable(5).expect("Unable disable cpu");
+    /// ```
+    pub fn disable(&self, id: usize) -> Result<(), CpuFreqError> {
         Ok(CPU::write_file(&format!("cpu{id}/online"), "0")?)
     }
-    fn enable_all(&self) -> Result<(), CpuFreqError> {
+    /// Enable all present cpus
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.enable_all().expect("Unable to enable all present cpus");
+    /// ```
+    pub fn enable_all(&self) -> Result<(), CpuFreqError> {
         for cpu in CPU::get_ranges("present")? {
             if cpu != 0 {
                 self.enable(cpu)?;
@@ -139,7 +287,16 @@ impl CPU {
         }
         Ok(())
     }
-    fn disable_all(&self) -> Result<(), CpuFreqError> {
+    /// Disable all present cpus
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.disable_all().expect("Unable to disable all present cpus");
+    /// ```
+    pub fn disable_all(&self) -> Result<(), CpuFreqError> {
         for cpu in CPU::get_ranges("present")? {
             if cpu != 0 {
                 self.disable(cpu)?;
@@ -147,7 +304,17 @@ impl CPU {
         }
         Ok(())
     }
-    fn disable_hyperthread(&self) -> Result<(), CpuFreqError> {
+    /// Disable all siblings threads
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// cpu.enable_all();
+    /// let freqs = cpu.disable_hyperthread().expect("Unable to disable hyperthread");
+    /// ```
+    pub fn disable_hyperthread(&self) -> Result<(), CpuFreqError> {
         let mut to_disable = HashSet::new();
         for cpu in CPU::get_ranges("online")? {
             let path = format!("cpu{cpu}/topology/thread_siblings_list");
@@ -159,7 +326,16 @@ impl CPU {
         }
         Ok(())
     }
-    fn reset(&self) -> Result<(), CpuFreqError> {
+    /// Reset cpu governor, max and min frequencies
+    ///
+    /// # Example
+    /// ```
+    /// use cpufreq::CPU;
+    ///
+    /// let cpu = CPU::new().unwrap();
+    /// let freqs = cpu.reset().expect("Unable to reset cpu");
+    /// ```
+    pub fn reset(&self) -> Result<(), CpuFreqError> {
         self.enable_all()?;
         self.set_governors("schedutil")?;
         let avail_freqs = self.available_frequencies()?;
@@ -245,6 +421,7 @@ mod tests {
     #[test]
     fn hyperthread() {
         let cpu = CPU::new().unwrap();
+        cpu.enable_all().unwrap();
         cpu.disable_hyperthread().unwrap();
     }
     #[test]
